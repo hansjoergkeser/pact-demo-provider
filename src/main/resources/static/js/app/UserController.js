96731c167595ae9d0de5a7c7c936db2e1fb7cacf
Ashish Lohia <ashishlohia70@gmail.com>
2017-09-13 22:19:11 +0530

    random enhancements

--- src/main/resources/static/js/app/UserController.js
+++ src/main/resources/static/js/app/UserController.js
@@ -19,20 +19,26 @@ module.controller("UserController", [ "$scope", "UserService",
 				console.log("no callback");
 			});
 
-			UserService.getAllUsers().then(function(value) {
-				console.log(value.data);
-			}, function(reason) {
-				console.log("error occured");
-			}, function(value) {
-				console.log("no callback");
-			});
-
 			$scope.saveUser = function() {
 				$scope.userDto.skillDtos = $scope.skills.map(skill => {
 					return {skillId: null, skillName: skill};
 				});
 				UserService.saveUser($scope.userDto).then(function() {
 					console.log("works");
+					UserService.getAllUsers().then(function(value) {
+						$scope.allUsers= value.data;
+					}, function(reason) {
+						console.log("error occured");
+					}, function(value) {
+						console.log("no callback");
+					});
+
+					$scope.skills = [];
+					$scope.userDto = {
+						userId : null,
+						userName : null,
+						skillDtos : []
+					};
 				}, function(reason) {
 					console.log("error occured");
 				}, function(value) {
