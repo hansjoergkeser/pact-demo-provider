a43696162ece861a3ea2c326582119fe1116b76c
Keser Hansjörg (IT-PTR-EXT - Extern) <hansjoerg.keser@sbb.ch>
2022-07-27 01:16:49 +0200

    added cat image, visible after button click

--- src/main/java/com/example/demo/BootDemoApplication.java
+++ src/main/java/com/example/demo/BootDemoApplication.java
@@ -1,15 +1,9 @@
 package com.example.demo;
 
-import com.example.demo.entity.Skill;
-import com.example.demo.entity.User;
 import com.example.demo.repository.UserRepository;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
-
-import javax.annotation.PostConstruct;
-import java.util.Arrays;
-import java.util.List;
 
 @SpringBootApplication
 public class BootDemoApplication {
@@ -20,15 +14,10 @@ public class BootDemoApplication {
 		SpringApplication.run(BootDemoApplication.class, args);
 	}
 
-	@PostConstruct
-	public void setupDbWithData() {
-		User user1 = new User("Ashish", null);
-		user1.setSkills(Arrays.asList(new Skill("java"), new Skill("js")));
+//	@PostConstruct
+//	public void setupDbWithData() {
+//		User user = new User("Hajo", Arrays.asList(new Skill("Pact"), new Skill("Watching Cat Videos")));
 //		userRepository.save(user);
-
-		User user2 = new User("Hajo", Arrays.asList(new Skill("Pact"), new Skill("Watching Cat Videos")));
-//		userRepository.save(user);
-
-		userRepository.saveAll(List.of(user1, user2));
-	}
+//		userRepository.saveAll(List.of(user));
+//	}
 }
